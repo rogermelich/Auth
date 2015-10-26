ba591fdf44229de7cba2a95f8b664bc3d518a60f
rogermelich <rogermelich@gmail.com>
2015-10-26 19:43:35 +0100

    Add Laravel ide helper class

--- config/app.php
+++ config/app.php
@@ -145,6 +145,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
