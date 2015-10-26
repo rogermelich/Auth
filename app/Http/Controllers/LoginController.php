1ab582fd388a452cb5c4d0720165676db92dcada
rogermelich <rogermelich@gmail.com>
2015-10-26 18:31:01 +0100

    Modified Controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,5 +8,17 @@ use App\Http\Controllers\Controller;
 
 class LoginController extends Controller
 {
-    //
+        public function postLogin(Request $request){
+
+            if ($this->login($request->email.$request->password)){
+            // Redirect to more
+                return redirect()->route('home');
+            }else {
+                //Redirect to back
+                return redirect()->route('auth.login');
+            }
+    }
+    public function getLogin(){
+        return view('login');
+    }
 }
