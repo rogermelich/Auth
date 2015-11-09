bd5e17dbc799b3b15c08cf50d7cc543c9c8f136b
rogermelich <rogermelich@gmail.com>
2015-11-09 21:00:23 +0100

    Add forms from test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -16,4 +16,61 @@ class ExampleTest extends TestCase
         $this->visit('/')
              ->see('Laravel 5');
     }
+
+    /**
+     *
+     *
+     * @return void
+     */
+    public function testLoginPage()
+    {
+        $this->visit('auth.login')
+            ->see('LOGIN');
+
+    }
+
+    /**
+     *
+     *
+     * @return void
+     */
+    public function testUserWithoutAccesToLogin()
+    {
+        $this->unlogged();
+        $this->visit('/resource')
+            ->seePages(route('auth.login'))
+            ->see('Login')
+            ->dontSee('Logout');
+
+    }
+
+    /**
+     *
+     *
+     * @return void
+     */
+    public function testUserWithoutAccesToResource()
+    {
+        $this->logged();
+        $this->visit('/resource')
+            ->seePageId('/resource');
+    }
+
+
+    private function logged(){
+        session::set('authenticated', true);
+    }
+
+    private function unlogged(){
+        session::set('authenticated', false);
+    }
+
+    public function testLoginPageHaveRegisterLink()
+    {
+        $this->visit('/login')
+            ->type('rogermelich@gmail.com', 'email')
+            ->type('123456789', 'password')
+            ->click('Register')
+            ->seePageIs('/register');
+    }
 }
