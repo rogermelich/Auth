5251af3bd12d110ccb6f7df10e3ce2387a6b410d
rogermelich <rogermelich@gmail.com>
2015-11-09 21:00:41 +0100

    Modified Home add link to login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -39,42 +39,27 @@
 <div class="container">
     <div class="content">
         <div class="title">Login</div>
-<<<<<<< HEAD
-
         <form method="post" action="{{ route('auth.postLogin')}}">
             <!-- //{!! csrf_token() !!} -->
-=======
-        <form method="post" action="{{route('auth.postLogin')}}">
-            {!! csrf_token() !!}
->>>>>>> 45934c70ccf0fc2868000427acc4c62f5f307a4e
             <div class="form-group">
                 <label for="email">Email address:</label>
                 <input type="email" class="form-control" id="email" name="email">
             </div>
             <div class="form-group">
                 <label for="password">Password:</label>
-<<<<<<< HEAD
-                <input type="password" class="form-control" id="email" name="password">
-=======
                 <input type="password" class="form-control" id="password" name="password">
->>>>>>> 45934c70ccf0fc2868000427acc4c62f5f307a4e
             </div>
             <div class="checkbox">
                 <label><input type="checkbox"> Remember me</label>
             </div>
-<<<<<<< HEAD
-            <button type="submit" class="btn btn-default">Submit</button>
-            <button type="reset" class="btn btn-default">Reset</button>
-=======
             <button type="submit" class="btn btn-default">Login</button>
-            <button type="reset" class="btn btn-default">Login</button>
->>>>>>> 45934c70ccf0fc2868000427acc4c62f5f307a4e
+            <button type="reset" class="btn btn-default">Reset</button>
         </form>
+
+        <p>No ets usuari encara?</p>
+
+        <a id="register" href={{route("auth.register")}}>Registra't</a>
     </div>
 </div>
 </body>
-<<<<<<< HEAD
 </html>
-=======
-</html>
->>>>>>> 45934c70ccf0fc2868000427acc4c62f5f307a4e
