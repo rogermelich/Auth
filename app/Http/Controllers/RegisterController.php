a8c8b0a9f709c4cd68933203396ae44579dda865
rogermelich <rogermelich@gmail.com>
2015-11-10 21:58:09 +0100

    Resolf errors

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -1,12 +1,27 @@
 <?php
 namespace App\Http\Controllers;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Input;
+
 class RegisterController extends Controller
 {
     public function getRegister()
     {
         return view('register');
     }
+
+    public function  postRegister(){
+
+        $user = new User();
+
+        $user->name = Input::get('name');
+        $user->email = Input::get('email');
+        $user->password = Input::get('password');
+
+        $user->save();
+    }
+
 }
